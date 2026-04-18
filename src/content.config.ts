ca51914d84f54a1d3cc4ef3064f091446e3c84c3
Alan Xiao <alanxiao44@gmail.com>
2026-04-18 16:07:11 -0400

    Remove blog stuff

--- src/content.config.ts
+++ src/content.config.ts
@@ -19,21 +19,6 @@ export type DeckBuildingNotes = {
     // Regalis Piece
 }
 
-const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
-	schema: ({ image }) =>
-		z.object({
-			title: z.string(),
-			description: z.string(),
-			// Transform string to Date object
-			pubDate: z.coerce.date(),
-			updatedDate: z.coerce.date().optional(),
-			heroImage: image().optional(),
-		}),
-});
-
 const decks = defineCollection({
     // Load Markdown and MDX files in the `src/content/blog/` directory.
     loader: glob({ base: './src/content/decks', pattern: '**/*.{md,mdx}' }),
@@ -59,4 +44,4 @@ const decks = defineCollection({
 });
 
 
-export const collections = { blog, decks };
+export const collections = { decks };
